b170f49290bfc60fe93ce9abf99bb8e61e1eea7d
iMac <imac@iMacs-iMac.local>
2025-07-29 13:31:07 +0530

    add google service file

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,8 @@
-    module.exports = {
-      project: {
-        ios: {},
-        android: {},
-      },
-      assets: ['./app/assets/fonts','./app/assets/images'], // Path to your fonts folder
-    };
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./app/assets/fonts', './app/assets/images'], // Path to your fonts folder
+  nodeBinary: '/Users/imac/.nvm/versions/node/v24.4.1/bin/node',
+};
